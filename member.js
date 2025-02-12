c8e5effcdf91a3f7b5e34731c677b0408760b14f
Dergül Bilginan <2433001079@ogrenci.sbu.edu.tr>
2025-02-12 12:21:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['html', 'css', 'js'],
+        showSkills: function() {
+            this.skills.forEach(function(skill) {
+                console.log(`${this.name} knows ${skill}`);
+            });
+        }
+    }
+
+    member.showSkills();
+}
